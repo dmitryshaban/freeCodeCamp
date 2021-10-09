5dbf628f5bb87b1e7a0861ffc139061a9f063f93
Dmitry <test@gmail.com>
2021-10-09 11:07:04 +0300

    lesson done

---
+++ lesson 6.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
