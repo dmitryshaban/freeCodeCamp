ca5f9408f3b659a60dad56ca7543b6b0afe331a4
Dmitry <mr.linoi@mail.ru>
2021-11-09 22:23:58 +0300

    1

---
+++ Basic JavaScript/confirm-the-ending.js
@@ -0,0 +1,8 @@
+function confirmEnding(str, target) {
+
+  return str.slice(str.length - target.length) === target;
+}
+
+confirmEnding("He has to give me a new name", "name");
+
+confirmEnding("Bastian", "n");
