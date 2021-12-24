8c51e19d5a1582fbeb7e299b7dc44a20190727b4
Dmitry <mr.linoi@mail.ru>
2021-12-24 18:46:48 +0300

    algoritm

---
+++ Basic JavaScript/spinal-tap-case.js
@@ -0,0 +1,12 @@
+function spinalCase(str) {
+
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  return str
+    .toLowerCase()
+    .split(/(?:_| )+/)
+    .join("-");
+}
+
+
+spinalCase("This Is Spinal Tap");
