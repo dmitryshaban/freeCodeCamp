21931e7c1e2fb31a8cd39c4a79183e4da2db8b45
Dmitry <mr.linoi@mail.ru>
2022-01-10 01:53:59 +0300

    functional

---
+++ Basic JavaScript/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,12 @@
+// the global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Add your code below this line
+  return fixedValue + 1;
+
+  // Add your code above this line
+}
+
+let newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
