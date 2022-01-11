ab6568b39fc5dcf1485278154ef052a33f1decf7
Dmitry <mr.linoi@mail.ru>
2022-01-11 12:37:22 +0300

    functional

---
+++ Basic JavaScript/implement-map-on-a-prototype.js
@@ -0,0 +1,18 @@
+// The global Array
+let s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  let newArray = [];
+
+  // Add your code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  // Add your code above this line
+
+  return newArray;
+};
+
+let new_s = s.myMap(function(item) {
+  return item * 2;
+});
