df0b45069be4f3d17664476fe59ca409496c8e38
Dmitry <mr.linoi@mail.ru>
2022-02-02 14:23:13 +0300

    functional

---
+++ Basic JavaScript/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,12 @@
+// the global variable
+let fixedValue = 4;
+
+// Add your code below this line
+function incrementer(value) {
+  return value + 1;
+
+  // Add your code above this line
+}
+
+let differentValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
